b247868383661261cc8c8e67ab530c172cbd4ff8
sgray3 <sgry50@uncc.edu>
2020-01-30 20:10:49 +0000

    hashes - Part 1

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -1,15 +1,24 @@
 # RubyHashes
 # Part I
 def array_2_hash emails, contacts
-  array = {name: 'Bob smith', email: 'bobsmith@example.com'}
-  array.each do|key,value|
-  puts "Bob's #{key} is #{value}" 
-  end 
+ emails = ["bobsmith@example.com","sallyfield@example.com","markdole@example.com"]
+ emails.each{|e|puts e}
+ persons = {name1: 'bob smith', contact1: 'bobsmith@example.com',
+            name2: 'sally field', contact2:'sallyfield@example.com',
+            name3: 'mark dole', contact3:'markdole@example.com'}
+  persons.each do|key,key1|
+  puts  "#{value} has #{key1}"
+  end  
 end
 
 # Part II
 def array2d_2_hash contact_info, contacts
-    # YOUR CODE HERE
+   array[[emails, contacts]]
+   
+   
+   
+   
+   
 end
 
 # Part III
