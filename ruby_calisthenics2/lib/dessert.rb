4de883347726e9e0b99c5af52043c3d4544886d9
sgray3 <sgry50@uncc.edu>
2020-03-10 10:39:21 +0000

    Initial Commit for Rails Basics 1

--- ruby_calisthenics2/lib/dessert.rb
+++ ruby_calisthenics2/lib/dessert.rb
@@ -1,19 +1,18 @@
 class Dessert
-  # add code for setters and getters
+  attr_accessor:name,:calories
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
-    # your code here
-  end
-  def delicious?
-    # your code here
-  end
-end
-
+    @calories < 200
+  end 
+  def delicious? 
+    true
+  end 
+end 
 class JellyBean < Dessert
-  # add code for setters and getters
+  
   def initialize(flavor)
-    # your code here
   end
-end
+end 
