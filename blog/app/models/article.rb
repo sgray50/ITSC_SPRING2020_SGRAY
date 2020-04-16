faf3e2a4fe5ef729a8ae1eb36085c7f55d0a0204
sgray3 <sgry50@uncc.edu>
2020-04-16 17:21:33 +0000

    Ading user functionality

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
-    has_many :comments
+    belongs_to :user
+    
+    has_many :comments, dependent: :destroy
+    
     validates :title, presence: true, length: { minimum: 5 }
     validates :title, presence: true, length: { minimum: 10 }
 end
